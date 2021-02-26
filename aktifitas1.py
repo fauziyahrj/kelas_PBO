62a9802eecdea116453fe216b7d17563f0104626
fauziyahrj <fauziyahrhaudha@gmail.com>
2021-02-26 09:19:09 +0700

    Fauziyah 1904035 Aktifitas 1

---
+++ aktifitas1.py
@@ -0,0 +1,10 @@
+#INPUT
+nama = input ("masukkan nama Anda : ")
+umur = 19
+
+#OUTPUT
+print("")
+print ("=====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PYTHON=====")
+print ("nama Anda : ", nama)
+print ("umur Anda : ", umur)
+print ("")
